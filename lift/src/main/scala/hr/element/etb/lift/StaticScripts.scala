a7d2a6bb20b22d53741700d50e45fadd906ba937
Marko Elezovic <marko@element.hr>
2012-01-11 15:17:27 +0100

    Added ETB Lift project (StaticScripts), depends on lift-webkit 2.4-RC1

---
+++ lift/src/main/scala/hr/element/etb/lift/StaticScripts.scala
@@ -0,0 +1,29 @@
+package hr.element.etb.lift.snippet
+
+import scala.xml.NodeSeq
+import net.liftweb.util.Props
+import net.liftweb.http.S
+import net.liftweb.common._
+
+abstract class StaticScripts(root: String) {
+  def serveScript(typ: String, ver: String, min: String) = {
+    val suffix = min match {
+      case "all" =>
+        ".min"
+      case "prod" if Props.productionMode =>
+        ".min"
+      case _ =>
+        ""
+    }
+
+    val path = "/%s/js/%s%s%s.js" format (root, typ, ver, suffix);
+    <script type="text/javascript" src={ path }/>
+  }
+
+  def render(n: NodeSeq): NodeSeq = {
+    val ver = S.attr("version").map("-"+).openOr("")
+    val min = S.attr("min").openOr("none")
+    val typ = S.attr("type").ensuring(_.isDefined, "Scripts type attribute not specified!").open_!
+    serveScript(typ, ver, min)
+  }
+}
