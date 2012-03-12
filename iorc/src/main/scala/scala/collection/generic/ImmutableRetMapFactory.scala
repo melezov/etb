ff075b2b6321f5a8fd03d33029b449746b682a94
Marko Elezovic <marko@element.hr>
2012-03-12 04:05:15 +0100

    Moved IORC to PGScala project.

--- iorc/src/main/scala/scala/collection/generic/ImmutableRetMapFactory.scala
+++
@@ -1,6 +0,0 @@
-/*
-package scala.collection
-package generic
-
-abstract class ImmutableRetMapFactory[CC[A] <: immutable.RetMap[A] with RetMapLike[A, CC[A]]] extends RetMapFactory[CC]
-*/
--- iorc/src/main/scala/scala/collection/generic/ImmutableRetSetFactory.scala
+++
@@ -1,8 +0,0 @@
-/*
-package scala.collection.generic
-
-import scala.collection.immutable.RetSet
-import scala.collection.RetSetLike
-
-abstract class ImmutableRetSetFactory[CC[A] <: RetSet[A] with RetSetLike[A, CC[A]]] extends RetSetFactory[CC]
-*/
