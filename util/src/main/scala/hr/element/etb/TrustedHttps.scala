a647449e91cc5e00691f144c866ccfd1da12398e
Deni Bertovic <deni@element.hr>
2012-06-21 16:50:08 +0200

    added keystore managers for handling client certificates to TrustedHttps

--- util/src/main/scala/hr/element/etb/TrustedHttps.scala
+++ util/src/main/scala/hr/element/etb/TrustedHttps.scala
@@ -8,12 +8,12 @@ import org.apache.http.conn.scheme.Scheme
 
 import dispatch._
 
-class TrustedHttps(truststore: String, truststorePassword: String) extends Http {
+class TrustedHttps(truststore: String, truststorePassword: String, ks: String = null, keystorePassword: String = null) extends Http {
   client.getConnectionManager.getSchemeRegistry.register(
     new Scheme("https", 443, sslSocketFactory)
   )
-
-  private lazy val trustManagers = {
+  
+  private lazy val trustManagers = { 
     val factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
     val keystore = KeyStore.getInstance(KeyStore.getDefaultType)
     val kS = classOf[TrustedHttps].getResourceAsStream(truststore)
@@ -21,10 +21,25 @@ class TrustedHttps(truststore: String, truststorePassword: String) extends Http 
     factory.init(keystore)
     factory.getTrustManagers
   }
+ 
 
-  private lazy val sslSocketFactory = {
+  private lazy val keyManagers = { 
+    val kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
+    val keystore = KeyStore.getInstance("PKCS12");
+    val kS = classOf[TrustedHttps].getResourceAsStream(ks)
+    keystore.load(kS, keystorePassword.toCharArray())
+    kmf.init(keystore, keystorePassword.toCharArray())
+    kmf.getKeyManagers
+  }   
+
+  private lazy val sslSocketFactory = { 
+    val km = ks match {
+      case null => null
+      case _ => keyManagers
+    }   
     val sslContext = javax.net.ssl.SSLContext.getInstance("TLS")
-    sslContext.init(null, trustManagers, new SecureRandom())
+    sslContext.init(km, trustManagers, new SecureRandom())
     new SSLSocketFactory(sslContext)
   }
 }
+
