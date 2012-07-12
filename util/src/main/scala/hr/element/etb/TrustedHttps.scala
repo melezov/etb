b3042e19baa0cf548b8833f967534b7cb5f506d0
Marin Vila <marin@element.hr>
2012-07-12 11:11:37 +0200

    Default missing.

--- util/src/main/scala/hr/element/etb/TrustedHttps.scala
+++ util/src/main/scala/hr/element/etb/TrustedHttps.scala
@@ -8,7 +8,19 @@ import org.apache.http.conn.scheme.Scheme
 
 import dispatch._
 
-class TrustedHttps(truststore: String, truststorePassword: String, ks: Option[String] = None, keystorePassword: Option[String] = None, Timeout: Option[Int] = None) extends Http {
+object TrustedHttps{
+  def apply() =
+    new TrustedHttps("","")
+}
+
+
+case class TrustedHttps protected(
+    truststore: String
+  , truststorePassword: String
+  , ks: Option[String] = None
+  , keystorePassword: Option[String] = None
+  , Timeout: Option[Int] = None
+  ) extends Http {
   client.getConnectionManager.getSchemeRegistry.register(
     new Scheme("https", 443, sslSocketFactory)
   )
@@ -22,8 +34,9 @@ class TrustedHttps(truststore: String, truststorePassword: String, ks: Option[St
       params.setParameter(SO_TIMEOUT, x)
     }
   }
-   
-  private lazy val trustManagers = { 
+
+
+  private lazy val trustManagers = {
     val factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
     val keystore = KeyStore.getInstance(KeyStore.getDefaultType)
     val kS = classOf[TrustedHttps].getResourceAsStream(truststore)
@@ -31,24 +44,38 @@ class TrustedHttps(truststore: String, truststorePassword: String, ks: Option[St
     factory.init(keystore)
     factory.getTrustManagers
   }
- 
-  private def keyManagers(k: String, p: String) = { 
+
+  private def keyManagers(k: String, p: String) = {
     val kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
     val keystore = KeyStore.getInstance("PKCS12");
     val kS = classOf[TrustedHttps].getResourceAsStream(k)
     keystore.load(kS, p.toCharArray())
     kmf.init(keystore, p.toCharArray())
     kmf.getKeyManagers
-  }   
+  }
 
-  private lazy val sslSocketFactory = { 
+  private lazy val sslSocketFactory = {
     val km = (ks,keystorePassword) match {
       case (None, None) => null
       case (Some(k),Some(p)) => keyManagers(k,p)
-    }   
+    }
     val sslContext = javax.net.ssl.SSLContext.getInstance("TLS")
     sslContext.init(km, trustManagers, new SecureRandom())
     new SSLSocketFactory(sslContext)
   }
+
+  def setTruststore(truststore: String, truststorePassword: String) =
+    copy(
+        truststore = truststore
+      , truststorePassword = truststorePassword
+      )
+
+  def setKeyStore(ks: Option[String], keystorePassword: Option[String]) =
+    copy( ks = ks
+        , keystorePassword = keystorePassword
+        )
+
+  def setTimeout(Timeout: Option[Int]) =
+    copy( Timeout = Timeout )
 }
 
